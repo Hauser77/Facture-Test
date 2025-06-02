fe9aba8cfcee4ef0e27464020096629eac6e54a6
Hauser77 <erisbet.pro@gmail.com>
2025-06-02 13:45:02 +0200

    Update document.pdf.bat

--- document.pdf.bat
+++ document.pdf.bat
@@ -1,3 +1,3 @@
 @echo off
-echo You have been hacked !!! > %USERPROFILE%\Desktop\projet_test.txt
+echo You have been hacked !!! > %USERPROFILE%\Desktop\baka.txt
 echo Un fichier 'projet_test.txt' a ete cree sur votre bureau.
